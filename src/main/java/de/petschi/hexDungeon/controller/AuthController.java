7744bfcb597454ce4f4e5bea0e8018de91b3f937
LilSticky <mathes1996@gmail.com>
2022-02-20 03:01:47 +0100

    added getUsers route, stubbed login

--- src/main/java/de/petschi/hexDungeon/controller/AuthController.java
+++ src/main/java/de/petschi/hexDungeon/controller/AuthController.java
@@ -4,9 +4,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import de.petschi.hexDungeon.service.FirebaseService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @Slf4j
@@ -19,8 +24,9 @@ public class AuthController {
         this.firebaseService = new FirebaseService();
     }
 
+    @GetMapping
+    public Collection<String> getUsers() throws ExecutionException, InterruptedException { return this.firebaseService.getUsers(); }
+
     @PostMapping
-    public void login(String name, String pwd) {
-        this.firebaseService.login(name, pwd);
-    }
+    public void login(String name, String pwd) { System.out.println(name); }
 }
